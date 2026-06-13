ec99347acfd5d00ff2d51774b5fe54080ae4218e
Fatahillah Satria Bima Seno <seno.fatahillahsatriabima@gmail.com>
2026-06-13 09:15:42 +0700

    Pemberian nilai default pada komponen

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import UserProfile from "./components/UserProfile/UserProfile"
+import UserProfile from "./components/UserProfile/UserProfile";
+
+const user = {
+  firstName: "John",
+  lastName: "Doe",
+  skills: "React, Javascript",
+};
+
+const user2 = {
+  firstName: "Mark",
+  lastName: "Doe",
+  skills: "NestJS, Typescript",
+};
 
 function App() {
-
   return (
     <>
-      <UserProfile />
+      <UserProfile firstName={user.firstName} lastName={user.lastName} />
+      <UserProfile
+        firstName={user2.firstName}
+        lastName={user2.lastName}
+        skills={user2.skills}
+      />
       <h1>Hello World</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/userProfile/userProfile.jsx
+++ src/components/userProfile/userProfile.jsx
@@ -1,15 +1,21 @@
-import styles from "./UserProfile.module.css"
+import styles from "./UserProfile.module.css";
 
-export default function UserProfile(){
+export default function UserProfile(props) {
+  const {
+    firstName = "First name not added",
+    lastName = "Last name not added",
+    skills = "Skills not added",
+  } = props;
+
   return (
     <>
-      <section className={`${styles['user-details']} ${styles.border}`}>
+      <section className={`${styles["user-details"]} ${styles.border}`}>
         <ul>
-          <li>First Name : John</li>
-          <li>Last Name : Doe</li>
-          <li>Skill : React, Javascript</li>
+          <li>First Name : {firstName}</li>
+          <li>Last Name : {lastName}</li>
+          <li>Skill : {skills}</li>
         </ul>
       </section>
     </>
-  )
+  );
 }
