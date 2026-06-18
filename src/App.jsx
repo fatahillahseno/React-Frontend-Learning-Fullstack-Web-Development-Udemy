7208719a60ab8c5017fa5389c58ed604441d3bed
Fatahillah Satria Bima Seno <seno.fatahillahsatriabima@gmail.com>
2026-06-18 13:59:20 +0700

    Pembersihan React App dan Instalasi React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,7 @@
-import UserProfile from "./components/UserProfile/UserProfile";
-
-const user = {
-  firstName: "John",
-  lastName: "Doe",
-  skills: "React, Javascript",
-};
-
-const user2 = {
-  firstName: "Mark",
-  lastName: "Doe",
-  skills: "NestJS, Typescript",
-};
-
 function App() {
   return (
     <>
-      <UserProfile firstName={user.firstName} lastName={user.lastName} />
-      <UserProfile
-        firstName={user2.firstName}
-        lastName={user2.lastName}
-        skills={user2.skills}
-      />
-      <h1>Hello World</h1>
+      <p>Hello World</p>
     </>
   );
 }
