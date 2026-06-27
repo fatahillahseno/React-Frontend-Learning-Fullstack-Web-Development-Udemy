6a8819aed3bf5e8bd7688fcd1929b59aa4d4adad
Fatahillah Satria Bima Seno <seno.fatahillahsatriabima@gmail.com>
2026-06-27 14:25:02 +0700

    Membuat TasksCounter component dan memanggilnya di Tasks.jsx

---
+++ src/components/TasksCounter/TasksCounter.jsx
@@ -0,0 +1,12 @@
+export default function TasksCounter() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="rounded-full border-solid border-4 border-red-700 p-6 mb-4">
+        <div className="flex justify-center min-w-10 min-h-10 text-white text-3xl text-center leading-10">
+          12
+        </div>
+      </div>
+      <div>Todo</div>
+    </div>
+  );
+}
