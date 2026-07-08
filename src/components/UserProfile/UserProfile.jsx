3920b5c1222556c879d523594b203239b7f2f1f6
Fatahillah Satria Bima Seno <seno.fatahillahsatriabima@gmail.com>
2026-07-08 09:10:33 +0700

    Konfigurasi UserProfile dan Avatar

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,3 +1,14 @@
-export default function UserProfile() {
-  return <div>User Profile</div>;
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+export default function UserProfile({ firstname = "John" } = props) {
+  return (
+    <div className="flex flex-col w-full py-8 items-center">
+      <Avatar className="mb-4">
+        <AvatarFallback className="text-2xl font-semibold">
+          {firstname.slice(0, 1)}
+        </AvatarFallback>
+      </Avatar>
+
+      <h4>Hello, {firstname}</h4>
+    </div>
+  );
 }
