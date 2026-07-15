86893161e4632be559e73fee1c52d4332988dc54
Fatahillah Satria Bima Seno <seno.fatahillahsatriabima@gmail.com>
2026-07-15 13:45:56 +0700

    Konfigurasi CreateTaskForm

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,14 +1,20 @@
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
-export default function UserProfile({ firstname = "John" } = props) {
+export default function UserProfile({
+  firstname = "John",
+  lastname = "Doe",
+} = props) {
   return (
-    <div className="flex flex-col w-full py-8 items-center">
+    <div className="flex flex-col w-full pt-8 items-center">
       <Avatar className="mb-4">
         <AvatarFallback className="text-2xl font-semibold">
           {firstname.slice(0, 1)}
+          {lastname.slice(0, 1)}
         </AvatarFallback>
       </Avatar>
 
-      <h4>Hello, {firstname}</h4>
+      <h4>
+        Hello, {firstname} {lastname}
+      </h4>
     </div>
   );
 }
