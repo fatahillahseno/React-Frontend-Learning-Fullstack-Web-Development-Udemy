4f50940196320750360b8f1a3e4fd52f54ddb374
Fatahillah Satria Bima Seno <seno.fatahillahsatriabima@gmail.com>
2026-08-03 12:04:34 +0700

    Pembuatan SignupSchema untuk komunikasi antara client app dan server app

---
+++ src/schema/signup.schema.js
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+const passwordValidation = new RegExp(
+  /^(?=.*\d)(?=.*[!@#$%^&*_])(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
+);
+
+export const SignupSchema = z.object({
+  firstName: z
+    .string()
+    .min(3, {
+      message: "First name must be at least 3 characters.",
+    })
+    .max(100, { message: "Fist name cannot be more than 100 characters." }),
+  lastName: z
+    .string()
+    .optional()
+    .max(100, { message: "Last name cannot be more than 100 characters." }),
+  email: z.string().email(),
+  password: z.string().regex(passwordValidation, {
+    message:
+      "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.",
+  }),
+});
