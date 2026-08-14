1f3d3179a8e559dc54aa2a65d9adddbc3200b70d
Fatahillah Satria Bima Seno <seno.fatahillahsatriabima@gmail.com>
2026-08-14 14:59:37 +0700

    Pembuatan CreateTaskSchema dan validating Create Task Form pada CreateTaskForm.jsx

--- src/components/CreateTaskForm/CreateTaskForm.jsx
+++ src/components/CreateTaskForm/CreateTaskForm.jsx
@@ -1,5 +1,3 @@
-import { Input } from "@/components/ui/input";
-
 import {
   Select,
   SelectTrigger,
@@ -14,11 +12,24 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import { Calendar as CalendarIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { format } from "date-fns";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { CreateTaskSchema } from "@/schema/createTask.schema.js";
 
 import { Textarea } from "@/components/ui/textarea";
 
@@ -27,78 +38,162 @@ import { useState } from "react";
 export default function CreateTaskForm() {
   const [date, setDate] = useState();
 
+  const form = useForm({
+    resolver: zodResolver(CreateTaskSchema),
+  });
+
+  function onSubmit(values) {
+    let dueDate = JSON.stringify(values.dueDate);
+    console.log(values);
+  }
+
   return (
     <div>
       <h2 className="text-xl mb-4">Create a new task</h2>
-      <form>
-        <div className="py-2">
-          <Input type="text" placeholder="Task Title" />
-        </div>
 
-        <div className="flex flex-row justify-between py-2">
-          <div className="w-full mr-2">
-            <Select>
-              <SelectTrigger>
-                <SelectValue placeholder="Status" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectItem value="todo">Todo</SelectItem>
-                  <SelectItem value="inProgress">In Progress</SelectItem>
-                </SelectGroup>
-              </SelectContent>
-            </Select>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="py-2">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      placeholder="Title"
+                      {...field}
+                      value={field.value ?? ""}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
           </div>
 
-          <div className="w-full ml-2">
-            <Select>
-              <SelectTrigger>
-                <SelectValue placeholder="Priority" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectItem value="low">Low</SelectItem>
-                  <SelectItem value="normal">Normal</SelectItem>
-                  <SelectItem value="high">High</SelectItem>
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-          </div>
-        </div>
-
-        <div className="py-2">
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button
-                variant={"outline"}
-                className={cn(
-                  "w-full justify-start text-left font-normal",
-                  !date && "text-muted-foreground",
+          <div className="flex flex-row justify-between py-2">
+            <div className="w-full mr-2">
+              <FormField
+                control={form.control}
+                name="status"
+                render={({ field }) => (
+                  <FormItem>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Status" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectItem value="todo">Todo</SelectItem>
+                          <SelectItem value="inProgress">
+                            In Progress
+                          </SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
                 )}
-              >
-                <CalendarIcon className="mr-4" />
-                {date ? format(date, "PPP") : <span>Due Date</span>}
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0">
-              <Calendar
-                mode="single"
-                selected={date}
-                onSelect={setDate}
-                initialFocus
               />
-            </PopoverContent>
-          </Popover>
-        </div>
+            </div>
 
-        <div className="py-2">
-          <Textarea placeholder="Task Description" />
-        </div>
+            <div className="w-full ml-2">
+              <FormField
+                control={form.control}
+                name="priority"
+                render={({ field }) => (
+                  <FormItem>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Priority" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectItem value="low">Low</SelectItem>
+                          <SelectItem value="normal">Normal</SelectItem>
+                          <SelectItem value="high">High</SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
 
-        <div className="py-2 flex justify-end">
-          <Button>Create Task</Button>
-        </div>
-      </form>
+          <div className="py-2">
+            <FormField
+              control={form.control}
+              name="dueDate"
+              render={({ field }) => (
+                <FormItem>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant={"outline"}
+                          className={cn(
+                            "w-full justify-start text-left font-normal",
+                            !date && "text-muted-foreground",
+                          )}
+                        >
+                          <CalendarIcon className="mr-4" />
+                          {field.value ? (
+                            format(field.value, "PPP")
+                          ) : (
+                            <span>Due Date</span>
+                          )}
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0">
+                      <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        disabled={(date) => date < new Date()}
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <div className="py-2">
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Textarea placeholder="Task Description" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <div className="py-2 flex justify-end">
+            <Button type="submit">Create Task</Button>
+          </div>
+        </form>
+      </Form>
     </div>
   );
 }
