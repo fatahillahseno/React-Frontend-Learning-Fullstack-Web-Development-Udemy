277667a30c5c2637182278729e60ca83471292d0
Fatahillah Satria Bima Seno <seno.fatahillahsatriabima@gmail.com>
2026-08-28 16:05:52 +0700

    Pembuatan private routes dan memindahkan tasks route ke dalamnya

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,8 +3,18 @@ import Login from "./pages/Login/Login.jsx";
 import Signup from "./pages/Signup/Signup.jsx";
 import Tasks from "./pages/Tasks/Tasks.jsx";
 import Error404 from "./pages/404/404.jsx";
+import PrivateRoutes from "@/components/PrivateRoutes/PrivateRoutes.jsx";
 
 export const router = createBrowserRouter([
+  {
+    element: <PrivateRoutes />,
+    children: [
+      {
+        path: "tasks",
+        element: <Tasks />,
+      },
+    ],
+  },
   {
     path: "/",
     element: <Login />,
@@ -12,10 +22,6 @@ export const router = createBrowserRouter([
   {
     path: "signup",
     element: <Signup />,
-  },
-  {
-    path: "tasks",
-    element: <Tasks />,
   },
   {
     path: "*",
