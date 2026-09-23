26d862c23b53ecc1b6427f154c1a1c164f880f96
Fatahillah Satria Bima Seno <seno.fatahillahsatriabima@gmail.com>
2026-09-23 09:36:26 +0700

    Membut 1. state untuk links dan meta dan menerapkan jika ada tasks data 2. function extractQueryString untuk mengambil nilai params pada satu url 3. variabel penampung params tiap Link yang dibutuhkan

--- src/components/TaskPagination/TaskPagination.jsx
+++ src/components/TaskPagination/TaskPagination.jsx
@@ -6,11 +6,31 @@ import {
   PaginationLink,
   PaginationNext,
 } from "@/components/ui/pagination";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { TasksContext } from "@/context/Tasks.context.jsx";
+
+export function extractQueryString(url) {
+  const parsedURL = new URL(url);
+  const params = new URLSearchParams(parsedURL.search);
+  return params;
+}
 
 export default function TaskPagination() {
   const { tasks, setTasks } = useContext(TasksContext);
+  const [links, setLinks] = useState();
+  const [meta, setMeta] = useState();
+
+  const previousPage = links
+    ? extractQueryString(links.previous).toString()
+    : "#";
+  const nextPage = links ? extractQueryString(links.next).toString() : "#";
+  const order = links ? extractQueryString(links.current).get("order") : "#";
+  useEffect(() => {
+    if (tasks) {
+      setLinks(tasks.pagination.links);
+      setMeta(tasks.pagination.meta);
+    }
+  }, [tasks]);
   return (
     <Pagination>
       <PaginationContent>
